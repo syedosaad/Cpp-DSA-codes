a4c5185e3f4c804422df618433ae69602e367c0c
Syed Saaduddin <70066507+syedosaad@users.noreply.github.com>
2026-08-18 12:53:59 +0530

    Create 410. Split Array Largest Sum(Book allocation problem).cpp

---
+++ 410. Split Array Largest Sum(Book allocation problem).cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isValid(vector<int>& arr, int n, int m, int maxAllowedPages){
+        int students = 1, pages = 0;
+        for(int i=0; i<n ;i++){
+            if(arr[i] > maxAllowedPages){
+                return false;
+            }
+            if(pages + arr[i] <= maxAllowedPages){
+            pages += arr[i];
+            } else {
+                students++;
+                pages = arr[i];
+            }
+        }
+        return students>m ? false : true;
+    }
+    
+    int splitArray(vector<int>& arr, int m) {
+        int n = arr.size();
+        if(m>n){
+            return -1;
+        }
+        int sum = 0;
+
+        for(int i =0;i<n;i++){
+            sum+=arr[i];
+        }
+
+        int ans = -1;
+        int st = 0, end = sum;
+
+        while(st <= end){
+            int mid = st + (end - st)/2;
+
+            if(isValid(arr,n,m,mid)){
+                ans = mid;
+                end = mid - 1;
+            } else {
+                st = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
