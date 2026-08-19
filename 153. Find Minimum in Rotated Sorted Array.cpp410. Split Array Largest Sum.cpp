10238ec574bf4beade7aa95d8438757b6d122c12
Syed Saaduddin <70066507+syedosaad@users.noreply.github.com>
2026-08-19 11:17:10 +0530

    Update and rename 410. Split Array Largest Sum.cpp to 153. Find Minimum in Rotated Sorted Array.cpp410. Split Array Largest Sum.cpp

---
+++ 153. Find Minimum in Rotated Sorted Array.cpp410. Split Array Largest Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int st = 0, end = n-1;
+        while(st<end){
+            int mid = st + (end-st)/2;
+            
+            if(nums[mid]>nums[end]){
+                st = mid + 1;
+            } else {
+                end = mid ;
+            }
+        }
+        return nums[st];
+    }
+};
