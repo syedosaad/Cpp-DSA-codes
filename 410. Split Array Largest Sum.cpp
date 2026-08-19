0206e2fed2dc4406959620411404902901451820
Syed Saaduddin <70066507+syedosaad@users.noreply.github.com>
2026-08-19 10:44:06 +0530

    Create 410. Split Array Largest Sum.cpp

---
+++ 410. Split Array Largest Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+
+        int ans = INT_MAX;
+        for(int i=0; i<n; i++){
+            
+            if(nums[i]<ans){
+                ans = nums[i];
+            }
+        }
+        return ans;
+    }
+};
