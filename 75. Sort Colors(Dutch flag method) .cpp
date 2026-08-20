b9f5147c9af3c5b8364f17bdb065b1dae6b76e4e
Syed Saaduddin <70066507+syedosaad@users.noreply.github.com>
2026-08-20 13:17:23 +0530

    Create 75. Sort Colors(Dutch flag method) .cpp

---
+++ 75. Sort Colors(Dutch flag method) .cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+     //Dutch flag approach   
+        int n = nums.size();
+        int l = 0, m = 0, h = n-1; 
+
+        while(m<=h){
+            if(nums[m]==0){
+                swap(nums[m],nums[l]);
+                m++;
+                l++;
+            } else if(nums[m]==1){
+                m++;
+            } else {
+                swap(nums[m],nums[h]);
+                h--;
+            }
+        }
+    }
+};
