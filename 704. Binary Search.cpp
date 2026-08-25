91ddbe2556afdb0624c480045254c995ab81901b
Syed Saaduddin <70066507+syedosaad@users.noreply.github.com>
2026-08-25 19:00:52 +0530

    Create 704. Binary Search.cpp

---
+++ 704. Binary Search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int st = 0, end = n-1;
+        while(st<=end){
+            int mid = st + (end-st)/2;
+            
+            if(target == nums[mid]){
+                return mid;
+            }
+            
+            if(target<nums[mid]){
+                end = mid - 1;
+            } else {
+                st = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
